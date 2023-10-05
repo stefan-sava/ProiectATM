8cb40c02ee47d4d3ace9375ff59a35a923e333dc
Stefan Sava <stefan@sava.ro>
2023-10-05 04:36:38 +0300

    Creata clasa User

---
+++ src/User.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class User {
+    private int cardNumber;
+    private String cardHolderName;
+    private int cardSecurityCode;
+
+    public User(int cardNumber, String cardHolderName, int cardSecurityCode) {
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+        this.cardSecurityCode = cardSecurityCode;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public int getCardSecurityCode() {
+        return cardSecurityCode;
+    }
+
+    public void setCardSecurityCode(int cardSecurityCode) {
+        this.cardSecurityCode = cardSecurityCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return getCardNumber() == user.getCardNumber() && getCardSecurityCode() == user.getCardSecurityCode() && Objects.equals(getCardHolderName(), user.getCardHolderName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCardNumber(), getCardHolderName(), getCardSecurityCode());
+    }
+}
